bbdf3780d8eca69d4e658aff9b90faf59b60f8a5
jonas <jonass309@hotmail.com>
2023-05-04 14:18:02 -0300

    fix: adding fields in models, serializers and fixing authentication in views

--- products/models.py
+++ products/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+import uuid
 
 class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,primary_key=True,editable=False)
     saler = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="products"
     )
@@ -9,6 +10,7 @@ class Product(models.Model):
     name = models.CharField(max_length=127)
     category = models.CharField(max_length=127)
     available = models.BooleanField()
+    price = models.FloatField(max_length=10, decimal_places=2, null=False)
 
 
 def __str__(self):
--- products/views.py
+++ products/views.py
@@ -2,18 +2,22 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 from .permissions import IsSalerOrReadOnly
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class ProductView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsSalerOrReadOnly]
 
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
     def perform_create(self, serializer):
-        serializer.save(saler_id=self.request.user)
+        serializer.save(saler=self.request.user)
 
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsSalerOrReadOnly]
+    
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsSalerOrReadOnly]
