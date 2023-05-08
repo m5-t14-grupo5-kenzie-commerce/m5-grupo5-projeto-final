743c72cc248b3e3fce8e991d6af2a51c42bd1de9
anaadx <anapaula.assisduarte.99@gmail.com>
2023-05-08 13:26:14 -0300

    feat: add products for tests

--- products/views.py
+++ products/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import Product
+from .serializers import ProductSerializer
+from .permissions import IsProductOwner, IsSalerOrReadOnly
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class ProductView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsSalerOrReadOnly]
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(saler=self.request.user)
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsProductOwner]
+
+    serializer_class = ProductSerializer
+
+
+class ProductRetrieveNameView(generics.RetrieveAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsProductOwner]
+
+    def get_queryset(self):
+        return Product.objects.filter(name__icontains=self.kwargs.get("name"))
+
+    serializer_class = ProductSerializer
+
+
+class ProductRetrieveCategoryView(generics.RetrieveAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsProductOwner]
+
+    def get_queryset(self):
+        return Product.objects.filter(category__iexact=self.kwargs.get("category"))
+
+    serializer_class = ProductSerializer
